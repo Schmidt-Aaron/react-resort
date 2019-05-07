113ada99e4659ddb50f906044f28528c069c7497
schmidt-aaron <aschmidt808@gmail.com>
2019-05-07 11:20:25 -0700

    navbar + hero + prettier

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,16 @@
-import React, {Component} from 'react'
-import {FaAlignRight} from 'react-icons/fa'
-import logo from '../images/logo.svg'
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import { FaAlignRight } from "react-icons/fa";
+import logo from "../images/logo.svg";
+import { Link } from "react-router-dom";
 
 export default class Nav extends Component {
   state = {
-    isOpen:false
-  }
+    isOpen: false
+  };
   handleToggle = () => {
-    this.setState({isOpen:!this.state.isOpen})
-  }
+    this.setState({ isOpen: !this.state.isOpen });
+  };
   render() {
-
     return (
       <nav className="navbar">
         <div className="nav-center">
@@ -19,9 +18,26 @@ export default class Nav extends Component {
             <Link to="/">
               <img src={logo} alt="Beach Resort" />
             </Link>
+            <button
+              type="button"
+              className="nav-btn"
+              onClick={this.handleToggle}
+            >
+              <FaAlignRight className="nav-icon" />
+            </button>
           </div>
+          <ul
+            className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/rooms">Rooms</Link>
+            </li>
+          </ul>
         </div>
       </nav>
-    )
+    );
   }
 }
